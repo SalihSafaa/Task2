f792ea5724ade06d7178ce32506c9c0f258e80d8
salih <salih.safaa@switch.com.iq>
2026-08-30 17:02:56 +0300

    Day 2, the libraries didn't fully install so i will finish tommorow

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductCatalogApi;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Product>().Property(p => p.Price).HasPrecision(18, 2);
+    }
+}
